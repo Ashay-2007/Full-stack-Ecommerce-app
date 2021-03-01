eb0b43ff9e18ac44b2a64dd914a54dbe18d9617f
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-03-01 21:11:46 +0530

    clean the app and wireframe the header

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            {/* logo on the left -> img */}
+            {/* Search box */}
+            {/* 3 links on the right */}
+            {/* Basket icon */}
+        </div>
+    )
+}
+
+export default Header
