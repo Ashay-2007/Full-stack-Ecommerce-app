328c724771d69d514b6ba8cf68cd3c300a69000b
Ashay2007 <ashaywanjarkar123@gmail.com>
2021-03-02 16:07:11 +0530

    added banner to homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from './Header';
+import Home from './Home';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header/>
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
@@ -18,7 +20,7 @@ function App() {
           {/* This is the default route */}
           <Route path="/">
             <Header/>
-            <h1>Home page</h1>
+            <Home/>
           </Route>
         </Switch>
       </div>
--- src/Header.js
+++ src/Header.js
@@ -21,8 +21,8 @@ function Header() {
             </div>
 
             <div className="header__nav">
-                <Link className="header__link">
-                    <div to="/login" className="header__option">
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
                         <span className="header__optionLineOne">Hello Sonny</span>
                         <span className="header__optionLineTwo">Sign In</span>
                     </div>
